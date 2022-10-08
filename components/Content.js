328bcd657fe60aaa3b79ab639a0bb46dcffdfd25
gabycode <86427446+gabycode@users.noreply.github.com>
2022-10-08 10:09:53 -0400

    Update Content.js

--- components/Content.js
+++ components/Content.js
@@ -1,13 +1,32 @@
 import React from "react"
 
 export default function Content(props) {
+    // console.log(props)
     return (
-        <div>
+        <div className="main">
             <img 
                 src={props.imageUrl}
+                className="travel-img"
             />
-            <div>
-                <p>{props.title}</p>
+            <div className="main-content">
+                <div className="location-div">
+                    <i className="fa-solid fa-location-dot fa-xs"></i>
+                    <p className="location">{props.location}</p>
+                    <a 
+                        href="{props.googleMapsUrl}"
+                        className="google-maps" 
+                        target="_blank">View on Google Maps</a>
+                </div>
+                
+                <h1 className="travel-title">{props.title}</h1>
+                
+                <div className="dates">
+                    <p className="start-date">{props.startDate} - </p>
+                    <p className="end-date">{props.endDate}</p>
+                </div>
+                
+                <p className="travel-description">{props.description}</p>
+                
             </div>
         </div>
     )
