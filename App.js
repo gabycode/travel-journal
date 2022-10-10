7e2129af42cbd9943be242b8a3e08b85ceb1c8c5
gabycode <86427446+gabycode@users.noreply.github.com>
2022-10-09 23:00:24 -0400

    Update App.js

--- App.js
+++ App.js
@@ -4,22 +4,24 @@ import Navbar from "./components/Navbar"
 import Content from "./components/Content"
 
 export default function App() {
-    // const content = data.map(item => {
-    //     return (
-    //         <Content
-    //             title={item.title}
-    //             img={item.imageUrl}
-    //         />
-    //     )
-    // })
-
+    const dataElements = data.map( d => {
+        return <Content 
+                    key={d.id}
+                    title={d.title} 
+                    location={d.location}
+                    googleMapsUrl={d.googleMapsUrl}
+                    startDate={d.startDate}
+                    endDate={d.endDate}
+                    description={d.description}
+                    imageUrl={d.imageUrl} 
+                />
+    })
     return (
-        <div className="container">
+        <div>
             <Navbar/>
-            <Content
-                imgUrl="https://source.unsplash.com/WLxQvbMyfas"
-                title="Mount Fuji"
-            />
+            <div className="container">
+                {dataElements}
+            </div>
         </div>
     )
 }
