d86f30c301c56a5714fbef550613f054e3d6624c
gabycode <86427446+gabycode@users.noreply.github.com>
2022-10-09 23:00:27 -0400

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Navbar() {
     return (
         <nav>
-            <i className="fa-solid fa-earth-americas"></i>
+            <i className="fa-solid fa-earth-americas fa-lg"></i>
             <h1 className="nav-title">my travel journal.</h1>
         </nav>
     )
